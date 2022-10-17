0c582f8c4dc6f93e75bfd1c56f2a4b02b763ef1a
yeon <sy423513@gmail.com>
2022-10-17 15:51:50 +0900

    cors설정 수정

--- src/main/java/com/sparos/uniquone/msagateway/MsaGatewayApplication.java
+++ src/main/java/com/sparos/uniquone/msagateway/MsaGatewayApplication.java
@@ -3,6 +3,11 @@ package com.sparos.uniquone.msagateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +15,23 @@ public class MsaGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsaGatewayApplication.class, args);
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedOrigin("http://localhost:3000");
+		corsConfiguration.addAllowedMethod("*");
+		corsConfiguration.setAllowCredentials(true);
+		source.registerCorsConfiguration("/**", corsConfiguration);
+		return source;
+	}
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		return new CorsWebFilter(corsConfigurationSource());
 	}
 
 }
