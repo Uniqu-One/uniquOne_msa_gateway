3a8cad1ff1bf0bb1e23a3b6e134a62bcea45ea0d
seongbeom7 <bum090877@gmail.com>
2022-10-27 14:11:59 +0900

    test

--- src/main/java/com/sparos/uniquone/msagateway/MsaGatewayApplication.java
+++ src/main/java/com/sparos/uniquone/msagateway/MsaGatewayApplication.java
@@ -25,7 +25,7 @@ public class MsaGatewayApplication {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.addAllowedHeader("*");
-		corsConfiguration.addAllowedOrigin("http://localhost:3000");
+		corsConfiguration.addAllowedOriginPattern("*");
 		corsConfiguration.addAllowedMethod("*");
 		corsConfiguration.setAllowCredentials(true);
 		source.registerCorsConfiguration("/**", corsConfiguration);
